2760962f682e09596a575b5887b1330c0eda4c0a
Harrison Harnisch <hharnisc@gmail.com>
2013-10-14 19:05:28 -0700

    remove key

--- meteor-ilove-location/server/meteor-ilove-location-server.js
+++ meteor-ilove-location/server/meteor-ilove-location-server.js
@@ -1,7 +1,7 @@
 instaStream = new Meteor.Stream('insta');
 
 if (Meteor.isServer) {
-  var instagram = Meteor.require('instagram').createClient('4fd7ebff4df246539cdb6aaa471499db', 'c5cb143ccacc42e0ac8e2fc9bf394ddb');
+  var instagram = Meteor.require('instagram').createClient('<client_id>', '<client_secret>');
   Meteor.setInterval(function () {
     instagram.media.popular(function (images, error) {
       _.each(_.first(images, 4), function(media) {
